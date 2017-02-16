bc0d0a761dded6a5424f7f2585dd6d2f4da7c746
mkindblom <marieki@kth.se>
2017-02-16 20:54:36 +0100

    Update Main.py

--- bin/Main.py
+++ bin/Main.py
@@ -1,25 +1,38 @@
-import Stats
-import Analyze
-import File_Output
-import sys
-import csv
-import json
+import Stats, Analyze, File_Output
+import Params as params
+import argparse, json
 
 if __name__ == '__main__':
-    tag = sys.argv[1]
+    parser = argparse.ArgumentParser(description='Run conbase')
+    parser.add_argument('--stats', nargs=5, metavar=("<snp path>", "<bam path>", "<reference path>", "<number of nodes>", "<output name>"))
+    parser.add_argument('--analyze', nargs=2, metavar=("<json path>", "<output name>"))
+    parser.add_argument('--params', nargs=1, metavar=("<params path>"))
+    args = parser.parse_args()
 
-    if tag == 'stats':
-        snps_path = sys.argv[2]
-        bam_paths = sys.argv[3]
-        reference_path = sys.argv[4]
-        nodes = int(sys.argv[5])
-        output_file_name = sys.argv[6]
+    if args.params is not None:
+        f = open(args.params[0], 'r')
+        lines = f.read().splitlines()
+        for line in lines:
+            attribute, _, value_str =  line.split()
+            try:
+                value = int(value_str)
+            except ValueError:
+                value = float(value_str)
+            setattr(params, attribute, value)
+
+    if args.stats is not None:
+        snps_path = args.stats[0]
+        bam_paths = args.stats[1]
+        reference_path = args.stats[2]
+        nodes = int(args.stats[3])
+        output_file_name = args.stats[4]
         Stats.stats(snps_path, bam_paths, reference_path, nodes, output_file_name)
         print("Done!", + '../results/' + output_file_name + ".json" )
 
-    elif tag == 'analyze':
-        json_path = sys.argv[2]
-        output_name = '../results/' + sys.argv[3]
+    if args.analyze is not None:
+        json_path = args.analyze[0]
+        # main_directory = os.path.dirname(os.path.realpath(__file__)) 
+        output_name =  '../results/' + args.analyze[1]
         f = open(json_path)
         samples_names = json.loads(f.readline().strip('\n'))['samples']
 
@@ -40,4 +53,3 @@ if __name__ == '__main__':
             html.write_site(site)
         phylip.write_sites(my_sites)
         html.close()
-
