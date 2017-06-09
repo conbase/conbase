36d9433c549ca902ccda6ddde84f57c982ee176f
mkindblom <marieki@kth.se>
2017-06-09 10:05:12 +0200

    new params

--- bin/Params.py
+++ bin/Params.py
@@ -1,26 +1,26 @@
 ############### Stats #################
 # Read
-fragment_length = 600
+fragment_length = 650
 mapping_quality = 20
 base_quality = 20
 # ALT
-dp_limit = 20
-alt_ratio_limit = 0.1
+dp_limit = 15
+alt_ratio_limit = 0.3
 sample_vote_limit = 2   
-vote_ratio_limit = 1.0
+vote_ratio_limit = 0.7
 snp_read_limit = 1
 # Indel
 indel_ratio = 0.01
 # Bulk
-bulk_ref_limit = 0.99 #this requires the remaining percent to be only a1 
+bulk_ref_limit = 0.91 #this requires the remaining percent to be only a1 
 ############### Analyze #################
-dp_ms_limit = 10
+dp_ms_limit = 9
 msp_ratio = 0.1
 snp_total_vote = 0.9
-msp_internal_ratio = 0.1
+msp_internal_ratio = 0.10
 
 sample_ms_vote_limit = 2
-vote_ms_ratio_limit = 0.1
+vote_ms_ratio_limit = 0.3
 
 ################ Misc ##################
 snp_nr_limit = 10
